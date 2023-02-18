ad10724f389044d0884ffc686804709da05f7680
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-02-18 23:12:32 +0330

    added UserService

---
+++ Spring-simple-project/src/main/java/com/example/Springsimpleproject/model/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.Springsimpleproject.model.service;
+
+import com.example.Springsimpleproject.model.entity.User;
+import com.example.Springsimpleproject.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save (User user) {
+        return userRepository.save(user);
+    }
+}
