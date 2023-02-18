55bad03f5bf4f78df503bc59f45c01a2e82ece15
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-02-18 22:54:33 +0330

    Initial commit

---
+++ Spring-simple-project/src/main/java/com/example/Springsimpleproject/SpringSimpleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springsimpleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleProjectApplication.class, args);
+	}
+
+}
---
+++ Spring-simple-project/src/test/java/com/example/Springsimpleproject/SpringSimpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springsimpleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
