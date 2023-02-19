7e4638f2b71791cdccb9e0a2be1ccad0b69c2401
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-02-20 00:06:02 +0330

    write save , update , delete , findAll , findById methods in OrderService and PaymentService

--- Spring-simple-project/src/main/java/com/example/Springsimpleproject/model/service/OrderService.java
+++ Spring-simple-project/src/main/java/com/example/Springsimpleproject/model/service/OrderService.java
@@ -1,8 +1,14 @@
 package com.example.Springsimpleproject.model.service;
 
+import com.example.Springsimpleproject.model.entity.Order;
+import com.example.Springsimpleproject.model.entity.User;
 import com.example.Springsimpleproject.model.repository.OrderRepository;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
@@ -13,5 +19,32 @@ public class OrderService {
         this.orderRepository = orderRepository;
     }
 
+    public Order save(Order order) {
+        return orderRepository.save(order);
+    }
 
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public void delete (Long id) {
+        if (!orderRepository.existsById(id)){
+            throw new IllegalStateException("id not exist");
+        }
+        orderRepository.deleteById(id);
+    }
+
+    public Order update(Long id , Order order) {
+        if (!orderRepository.existsById(id)) {
+            throw new IllegalStateException("id not exist");
+        }
+        return orderRepository.save(order);
+    }
+
+    public Optional<Order> findById(Long id) {
+        if (!orderRepository.existsById(id)) {
+            throw new IllegalStateException("id not exist");
+        }
+        return orderRepository.findById(id);
+    }
 }
--- Spring-simple-project/src/main/java/com/example/Springsimpleproject/model/service/PaymentService.java
+++ Spring-simple-project/src/main/java/com/example/Springsimpleproject/model/service/PaymentService.java
@@ -1,8 +1,12 @@
 package com.example.Springsimpleproject.model.service;
 
+import com.example.Springsimpleproject.model.entity.Payment;
 import com.example.Springsimpleproject.model.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PaymentService {
@@ -12,4 +16,34 @@ public class PaymentService {
     public PaymentService(PaymentRepository paymentRepository) {
         this.paymentRepository = paymentRepository;
     }
+
+    public Payment save(Payment payment) {
+        return paymentRepository.save(payment);
+    }
+
+    public List<Payment> findAll() {
+        return paymentRepository.findAll();
+    }
+
+    public void delete(Long id) {
+        if (!paymentRepository.existsById(id)) {
+            throw new IllegalStateException("id not exist");
+        }
+        paymentRepository.deleteById(id);
+    }
+
+    public Payment update(Long id , Payment payment) {
+        if (!paymentRepository.existsById(id)) {
+            throw new IllegalStateException("id not exist");
+        }
+        return paymentRepository.save(payment);
+    }
+
+    public Optional<Payment> findById(Long id) {
+        if (!paymentRepository.existsById(id)) {
+            throw new IllegalStateException("id not exist");
+        }
+        return paymentRepository.findById(id);
+    }
 }
+
